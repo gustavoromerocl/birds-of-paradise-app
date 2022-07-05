c4b4e5d47f85688e25f490e3fd534cd4c50012f8
romerodevelopments <gandresrp@gmail.com>
2022-07-05 08:06:52 -0400

    configurar la base de datos

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+const db = require('./config/database');
+let app = express();
+
+db.connect;
 
 app.use(logger('dev'));
 app.use(express.json());
